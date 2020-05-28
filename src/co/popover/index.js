1348b161d4658be3de13fe574dc41cf7de523b69
Exentrich <exentrich@MacBook-Pro-Exentrich.local>
2020-05-28 09:42:54 +0300

    Popovers fixes

--- src/co/popover/index.js
+++ src/co/popover/index.js
@@ -5,18 +5,20 @@ import Context from './context'
 //save mouse position
 let _mousePos = { x:-1, y:-1 }
 document.body.addEventListener('mousemove', function(e){
-    _mousePos = {
-        x: e.pageX,
-        y: e.pageY
-    }
+    _mousePos = { x: e.pageX, y: e.pageY }
 })
+
+//last popover position
+let _lastPos = { x:-1, y:-1 }
 
 export default class Popover extends React.Component {
     static defaultProps = {
         onClose: undefined      //func, required
     }
 
-    _body = React.createRef()
+    _container = React.createRef()
+
+    initPos = _lastPos //when popover called from another popover, it's useful to show it on the same pos as previous
 
     store = {
         close: ()=>{
@@ -25,21 +27,33 @@ export default class Popover extends React.Component {
     }
     
     componentDidMount() {
-        this.updatePosition()
+        this._resizeObserver = new ResizeObserver(this.updatePosition)
+        this._resizeObserver.observe(this._container.current)
 
+        window.addEventListener('resize', this.updatePosition)
         document.body.addEventListener('mousedown', this.onBodyMouseDown)
         document.body.addEventListener('keydown', this.onBodyKeyDown)
     }
 
     componentWillUnmount() {
+        _lastPos = {x:-1, y:-1}
+
+        if (this._resizeObserver){
+            if (this._container.current)
+                this._resizeObserver.unobserve(this._container.current)
+            this._resizeObserver.disconnect()
+        }
+
+        window.removeEventListener('resize', this.updatePosition)
         document.body.removeEventListener('mousedown', this.onBodyMouseDown)
         document.body.removeEventListener('keydown', this.onBodyKeyDown)
     }
 
+    //click outside
     onBodyMouseDown = (e)=>{
         const { clientX, clientY } = e
-        const { offsetWidth, offsetHeight } = this._body.current
-        const { left, top } = this._body.current.getBoundingClientRect()
+        const { offsetWidth, offsetHeight } = this._container.current
+        const { left, top } = this._container.current.getBoundingClientRect()
         const mouseOver = ((clientX > left) && (clientX < (left+offsetWidth)) && (clientY > top) && (clientY < (top+offsetHeight)))
 
         if (!mouseOver)
@@ -56,25 +70,31 @@ export default class Popover extends React.Component {
     }
 
     updatePosition = ()=>{
-        //use current mouse position
-        let y = _mousePos.y
-        let x = _mousePos.x
-        
-        //pin to active element, if it over mouse pos
-        try{
-            const { left, top, width, height } = document.activeElement.getBoundingClientRect()
-            if (width < 200 && height < 200){
-                if (top < y && top + height >= y)
-                    y = top + height
+        if (this.initPos.x==-1) {
+            this.initPos = {}
 
-                if (left < x && left + width >= x)
-                    x = left
-            }
-        }catch(e){}
+            //use current mouse position
+            this.initPos.y = _mousePos.y
+            this.initPos.x = _mousePos.x
+
+            //pin to active element, if it over mouse pos
+            try{
+                const { left, top, width, height } = document.activeElement.getBoundingClientRect()
+                if (width < 200 && height < 200){
+                    if (top < this.initPos.y && top + height >= this.initPos.y)
+                        this.initPos.y = top + height
+
+                    if (left < this.initPos.x && left + width >= this.initPos.x)
+                        this.initPos.x = left
+                }
+            }catch(e){}
+        }
+
+        let { y, x } = this.initPos
 
         //prevent showing outside of viewport
         const { innerWidth, innerHeight } = window
-        const { offsetWidth, offsetHeight } = this._body.current
+        const { offsetWidth, offsetHeight } = this._container.current
 
         if (x + offsetWidth > innerWidth)
             x = innerWidth - offsetWidth - 10
@@ -86,7 +106,10 @@ export default class Popover extends React.Component {
         if (y < 0)
             y = 10
 
-        this._body.current.setAttribute('style', `top: ${y}px; left: ${x}px;`)
+        this._container.current.setAttribute('style', `top: ${y}px; left: ${x}px;`)
+
+        //save to global variable
+        _lastPos = { y, x }
     }
 
     render() {
@@ -94,7 +117,7 @@ export default class Popover extends React.Component {
             <Portal>
                 <Context.Provider value={this.store}>
                     <div
-                        ref={this._body}
+                        ref={this._container}
                         className='pop-body'>
                         {this.props.children}
                     </div>
