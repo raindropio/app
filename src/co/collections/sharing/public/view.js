23819402911f06b43898cb2ba25b3d2a70113802
Exentrich <exentrich@MacBook-Pro-Exentrich.local>
2020-07-15 12:54:49 +0300

    RSS

--- src/co/collections/sharing/public/view.js
+++ src/co/collections/sharing/public/view.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import t from '~t'
 
-import { Layout, Text, Checkbox, Title } from '~co/common/form'
+import { Layout, Text, Checkbox, Label } from '~co/common/form'
 import Button from '~co/common/button'
 
 export default class CollectionSharingPublic extends React.Component {
-    input = React.createRef()
+    url = React.createRef()
+    rss = React.createRef()
 
-    onCopyClick = () =>{
-        this.input.current.select()
-        this.input.current.setSelectionRange(0, 99999)
+    onCopyURLClick = () =>{
+        this.url.current.select()
+        this.url.current.setSelectionRange(0, 99999)
+        document.execCommand('copy')
+    }
+
+    onCopyRSSClick = () =>{
+        this.rss.current.select()
+        this.rss.current.setSelectionRange(0, 99999)
         document.execCommand('copy')
     }
 
@@ -18,31 +25,44 @@ export default class CollectionSharingPublic extends React.Component {
 
         return (
             <Layout>
-                <Title>
-                    {t.s('public')} URL
-                </Title>
-                
                 <div>
                     <Checkbox 
                         checked={etc.public}
                         disabled={access.level < 3}
                         onChange={onPublicClick}>
-                        {t.s('access')} {t.s('accessViaLink').toLowerCase()}
+                        {t.s('access')} {t.s('accessViaLink').toLowerCase()} / RSS
                     </Checkbox>
 
                     {etc.public && (
-                        <Text 
-                            ref={this.input}
-                            type='text'
-                            readOnly
-                            autoFocus
-                            value={`https://raindrop.io/collection/${_id||''}`}>
-                            <Button 
-                                variant='link'
-                                onClick={this.onCopyClick}>
-                                {t.s('copyLink')}
-                            </Button>
-                        </Text>
+                        <>
+                            <Label>URL</Label>
+                            <Text 
+                                ref={this.url}
+                                type='text'
+                                readOnly
+                                autoFocus
+                                value={`https://raindrop.io/collection/${_id||''}`}>
+                                <Button 
+                                    variant='link'
+                                    onClick={this.onCopyURLClick}>
+                                    {t.s('copyURL')}
+                                </Button>
+                            </Text>
+
+                            <Label>RSS</Label>
+                            <Text 
+                                ref={this.rss}
+                                type='text'
+                                readOnly
+                                autoFocus
+                                value={`https://raindrop.io/collection/${_id||''}/feed`}>
+                                <Button 
+                                    variant='link'
+                                    onClick={this.onCopyRSSClick}>
+                                    {t.s('copyURL')}
+                                </Button>
+                            </Text>
+                        </>
                     )}
                 </div>
             </Layout>
