44d742716a87e678b5bfb1dd460ea67906a82989
Exentrich <exentrich@MacBook-Pro-Exentrich.local>
2020-07-31 14:55:24 +0300

    Show filters/tags counters

--- src/co/search/suggestions.js
+++ src/co/search/suggestions.js
@@ -1,6 +1,7 @@
 import s from './suggestions.module.styl'
 import React from 'react'
 import t from '~t'
+import _ from 'lodash'
 import { createPortal } from 'react-dom'
 import { connect } from 'react-redux'
 import { makeFiltersSearch, getStatus } from '~data/selectors/filters'
@@ -8,7 +9,6 @@ import { autoLoad } from '~data/actions/filters'
 import { makeTagsSearch } from '~data/selectors/tags'
 
 import Button from '~co/common/button'
-import Icon from '~co/common/icon'
 import FilterIcon from '~co/filters/item/icon'
 import FilterTitle from '~co/filters/item/title'
 
@@ -18,6 +18,11 @@ class SearchSuggestions extends React.Component {
         spaceId: 0,
         floating: false,
         downshift: {}
+    }
+
+    sections = {
+        tags: <div key='_tags' className={s.section}>{t.s('tags')}</div>,
+        filters: <div key='_filter' className={s.section}>{_.capitalize(t.s('fastFilter'))}</div>
     }
 
     componentDidMount() {
@@ -50,8 +55,9 @@ class SearchSuggestions extends React.Component {
                         className: s.item,
                         variant: highlightedIndex === index ? 'primary' : 'outline'
                     })}>
-                    {type == 'filters' ? <FilterIcon {...item} /> : <Icon name='tag' size='micro' />}
+                    {type == 'filters' && <FilterIcon {...item} />}
                     {type == 'filters' ? <FilterTitle {...item} /> : item._id}
+                    {item.count > 1 && <span className={s.count}>{item.count}</span> }
                 </Button>
             )
         })
@@ -62,9 +68,9 @@ class SearchSuggestions extends React.Component {
         const filters = this.renderGroup('filters', tags.length)
 
         return [
-            ...(this.props.floating && tags.length ? [<div key='_tags' className={s.section}>{t.s('tags')}</div>] : []),
+            ...(this.props.floating && tags.length ? [this.sections.tags] : []),
             ...tags,
-            ...(this.props.floating && filters.length ? [<div key='_filter' className={s.section}>{t.s('fastFilter')}</div>] : []),
+            ...(this.props.floating && filters.length ? [this.sections.filters] : []),
             ...filters
         ]
     }
