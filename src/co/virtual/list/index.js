b660ca13863e3e887072dcbdffe475db360ae7c6
Exentrich <exentrich@MacBook-Pro-Exentrich.local>
2020-08-07 18:41:37 +0300

    Fix bug when virtual scroll jump up when reorder is enabled

--- src/co/virtual/list/index.js
+++ src/co/virtual/list/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Base from './base'
-import Sortable from './sortable'
+import Sortable from '../helpers/sortable'
 
 export default class VirtualList extends React.Component {
     static defaultProps = {
@@ -8,11 +8,27 @@ export default class VirtualList extends React.Component {
         //same as ./sortable
     }
 
-    render() {
-        const Component = this.props.sortEnabled ? Sortable : Base
+    state = {
+        sortableDataKey: ''
+    }
 
+    onSortableForceRerender = ()=>
+        this.setState({sortableDataKey: new Date().getTime()})
+
+    renderListContainer = container=>(
+        <Sortable
+            {...this.props}
+            {...container}
+            totalCount={this.props.totalCount+(this.props.footer?1:0)}
+            onForceRerender={this.onSortableForceRerender} />
+    )
+
+    render() {
         return (
-            <Component {...this.props} />
+            <Base 
+                {...this.props}
+                dataKey={this.props.dataKey+this.state.sortableDataKey}
+                ListContainer={this.props.sortEnabled ? this.renderListContainer : undefined} />
         )
     }
 }
