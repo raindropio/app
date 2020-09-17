e918b330051d17009c8c9ec6a91798a31616ae72
Rustem Mussabekov <winlinx@ya.ru>
2020-09-17 16:35:30 +0300

    Improve auto save to last collection

--- src/data/sagas/config.js
+++ src/data/sagas/config.js
@@ -3,12 +3,12 @@ import { put, takeLatest } from 'redux-saga/effects'
 import { CONFIG_SET_LASTCOLLECTION } from '../constants/config'
 import { USER_UPDATE_REQ } from '../constants/user'
 import { COLLECTION_CHANGE_VIEW } from '../constants/collections'
-import { SPACE_CHANGE_SORT, BOOKMARK_DRAFT_COMMIT } from '../constants/bookmarks'
+import { SPACE_CHANGE_SORT, BOOKMARK_CREATE_SUCCESS, BOOKMARK_UPDATE_SUCCESS } from '../constants/bookmarks'
 
 export default function* () {
     //last_collection
     yield takeLatest(CONFIG_SET_LASTCOLLECTION, userUpdate('spaceId', 'last_collection'))
-    yield takeLatest(BOOKMARK_DRAFT_COMMIT, userUpdate('item.collectionId', 'last_collection'))
+    yield takeLatest([BOOKMARK_CREATE_SUCCESS, BOOKMARK_UPDATE_SUCCESS], userUpdate('item.collectionId', 'last_collection'))
 
     yield takeLatest(COLLECTION_CHANGE_VIEW, userUpdate('view', 'raindrops_view'))
 
@@ -17,13 +17,16 @@ export default function* () {
 
 const userUpdate = function(from, to) {
     return function* (action) {
-        yield put({
-            type: USER_UPDATE_REQ, 
-            user: {
-                config:{
-                    [to]: _.at(action, from)[0]
+        const val = _.at(action, from)[0]
+
+        if (typeof val != 'undefined')
+            yield put({
+                type: USER_UPDATE_REQ, 
+                user: {
+                    config:{
+                        [to]: val
+                    }
                 }
-            }
-        })
+            })
     }
 }
