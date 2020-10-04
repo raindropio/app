478b393e75f63bf12c93eac786b645eb03491074
Rustem Mussabekov <winlinx@ya.ru>
2020-10-04 10:43:37 +0300

    Rewrite old ?is_clipper links

--- src/routes/notFound.js
+++ src/routes/notFound.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 import { target } from '~target'
 
-export default ({ location: { hash } })=>{
+export default ({ location: { hash='', search='' } })=>{
 	switch(target){
 		case 'extension':
 			return <Redirect to='/extension' />
 
 		case 'web':
+			//rewrite links of old app
+			if (search.includes('is_clipper')){
+				window.location.href=`https://extension.raindrop.io/${search}`
+				return null
+			}
+
 			if (hash.startsWith('#/'))
 				return <Redirect to={hash.replace(/^#/, '')} />
 		break
