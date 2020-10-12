f516be73ea9917347ba407de8006b10f99725ce8
Rustem Mussabekov <winlinx@ya.ru>
2020-10-12 10:29:04 +0300

    Show error message for user connect

--- src/routes/settings/account/connect.js
+++ src/routes/settings/account/connect.js
@@ -12,37 +12,69 @@ import { Label, Checkbox } from '~co/common/form'
 import Icon from '~co/common/icon'
 import Alert from '~co/common/alert'
 
-function SettingsProfileConnect({ user, location: { pathname } }) {
-    return (
-        <>
-            <Label>{t.s('signInSocial')}</Label>
-            
-            {target == 'web' ? (
-                <div>
-                    {['google', 'apple', 'facebook', 'twitter', 'vkontakte'].map(key=>{
-                        const enabled = user[key] && user[key].enabled
+class SettingsProfileConnect extends React.Component {
+    renderError = ()=>{
+        const { location: { search } } = this.props
+        const { connect_error='' } = Object.fromEntries(new URLSearchParams(search))||{}
 
-                        return (
-                            <Checkbox 
-                                key={key}
-                                checked={enabled}
-                                onChange={()=>window.location=`${API_ENDPOINT_URL}user/connect/${key}/${enabled ? 'revoke' : ''}`}
-                                className={enabled ? s.enabled : s.default}>
-                                <Icon 
-                                    name={key}
-                                    className={s.icon+' '+s[key]} />
-                                {_.capitalize(key)}
-                            </Checkbox>
-                        )
-                    })}
-                </div>
-            ) : (
-                <Alert variant='warning'>
-                    Please <a href={`${config.links.app.index}${pathname}`} target='_blank'>open web app</a> to configure social accounts!
-                </Alert>
-            )}
-        </>
-    )
+        if (!connect_error) return null
+
+        let content
+
+        switch(connect_error) {
+            case 'conflict':
+                content = <span>Can't link this account, because it already used by another user. Unlink it first and try again.</span>;
+                break
+
+            default: 
+                content = connect_error;
+                break
+        }
+
+        return (
+            <Alert variant='danger'>
+                {content}
+            </Alert>
+        )
+    }
+
+    render() {
+        const { user, location: { pathname, search } } = this.props
+        const { connect_error='' } = Object.fromEntries(new URLSearchParams(search))||{}
+
+        return (
+            <>
+                <Label>{t.s('signInSocial')}</Label>
+                
+                {target == 'web' ? (
+                    <div>
+                        {this.renderError()}
+
+                        {['google', 'apple', 'facebook', 'twitter', 'vkontakte'].map(key=>{
+                            const enabled = user[key] && user[key].enabled
+    
+                            return (
+                                <Checkbox 
+                                    key={key}
+                                    checked={enabled}
+                                    onChange={()=>window.location=`${API_ENDPOINT_URL}user/connect/${key}/${enabled ? 'revoke' : ''}`}
+                                    className={enabled ? s.enabled : s.default}>
+                                    <Icon 
+                                        name={key}
+                                        className={s.icon+' '+s[key]} />
+                                    {_.capitalize(key)}
+                                </Checkbox>
+                            )
+                        })}
+                    </div>
+                ) : (
+                    <Alert variant='warning'>
+                        Please <a href={`${config.links.app.index}${pathname}`} target='_blank'>open web app</a> to configure social accounts!
+                    </Alert>
+                )}
+            </>
+        )
+    }
 }
 
 export default connect(
