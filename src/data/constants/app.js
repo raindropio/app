be1f59251e53affbc98775b5eb017e16b65ab995
Rustem Mussabekov <winlinx@ya.ru>
2021-12-18 21:30:43 +0300

    Temporary use gumlet images

--- src/data/constants/app.js
+++ src/data/constants/app.js
@@ -3,12 +3,12 @@ export const
 
 export const
 	APP_BASE_URL 		= `https://${APP_DOMAIN}`,
-	WORKERS_BASE_URL	= 'https://rdl.ink',
-	LEGACY_WORKERS_BASE_URL=`https://stella.${APP_DOMAIN}`
+	WORKERS_BASE_URL	= 'https://rd-render-proxy.gumlet.io',
+	LEGACY_WORKERS_BASE_URL=`https://rdl.ink`
 
 export const
 	API_ENDPOINT_URL 	= `${process.env.NODE_ENV == 'production' || RAINDROP_ENVIRONMENT == 'react-native' ? 'https://api.raindrop.io' : 'http://localhost:3000' }/v1/`,
 	API_RETRIES 		= 3,
 	API_TIMEOUT 		= 30000,
 	FAVICON_URL 		= `${WORKERS_BASE_URL}/favicon`,
-	RENDER_URL 			= `${WORKERS_BASE_URL}/render`
+	RENDER_URL 			= `${WORKERS_BASE_URL}/fetch`
