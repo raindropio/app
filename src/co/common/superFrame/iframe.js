81bf2ae9184f5e4bb09b76dabb98448cd6efa1fb
Rustem Mussabekov <winlinx@ya.ru>
2021-12-20 15:57:44 +0300

    Fix web preview for files

--- src/co/common/superFrame/iframe.js
+++ src/co/common/superFrame/iframe.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react'
+import { API_ENDPOINT_URL } from '~data/constants/app'
 import iframeable from '~data/modules/format/iframeable_url'
 
 export default function Iframe({ src, onError, ...etc }) {
     const [loadSrc, setLoadSrc] = useState('')
 
     useEffect(()=>{
+        //link to file? preview is allowed so no need to check iframeable
+        if (src.includes(API_ENDPOINT_URL)) {
+            setLoadSrc(src)
+            return
+        }
+
         iframeable(src)
             .then(result=>{
                 if (result)
