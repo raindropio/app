5b5544d75dabb7e191ebea854929993e38a015a1
Rustem Mussabekov <winlinx@ya.ru>
2022-05-19 11:45:43 +0300

    Show collection title/icon in browser tab

--- src/routes/my/space/index.js
+++ src/routes/my/space/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Outlet, useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import { useSelector } from 'react-redux'
+import { makeCollection } from '~data/selectors/collections'
 
 import AccentColor from '~co/collections/item/accentColor'
 import Main, { Content } from '~co/screen/splitview/main'
@@ -10,8 +13,16 @@ import Bookmarks from './bookmarks'
 export default function PageMySpace() {
     const params = useParams()
 
+    const getCollection = useMemo(makeCollection, [])
+    const { title, cover=[] } = useSelector(state=>getCollection(state, params.cId))
+
     return (
         <AccentColor _id={params.cId}>{style=>(<>
+            <Helmet>
+                <title>{title}</title>
+                {cover.length ? <link rel='icon' href={cover[0]} /> : null}
+            </Helmet>
+
             <Main style={style}>
                 <Header {...params} />
                 <SearchIn {...params} />
