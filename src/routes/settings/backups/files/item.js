e006929f747bcb082f54b81cb04d28e7cfb199a2
Rustem Mussabekov <winlinx@ya.ru>
2022-06-12 12:03:44 +0300

    Better icons for automatic backups

--- src/routes/settings/backups/files/item.js
+++ src/routes/settings/backups/files/item.js
@@ -9,18 +9,18 @@ import Button, { ButtonsGroup } from '~co/common/button'
 export default function SettingsBackupsFilesItem({ created, files }) {
     return (
         <Item>
-            <ItemIcon><Icon name='download' /></ItemIcon>
+            <ItemIcon><Icon name='cache_ready' /></ItemIcon>
             <ItemTitle><LongDate date={created}/></ItemTitle>
 
             <ItemActions>
                 <ButtonsGroup>
                     <Button href={files.html} download>
-                        <Icon name='document' size='micro' />
+                        <Icon name='download' size='micro' />
                         {t.s('download')} HTML
                     </Button>
 
                     <Button href={files.csv} download>
-                        <Icon name='document' size='micro' />
+                        <Icon name='download' size='micro' />
                         {t.s('download')} CSV
                     </Button>
                 </ButtonsGroup>
