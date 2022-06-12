5fa58f1baa62b30225c0feba248d0b3897ca3eba
Rustem Mussabekov <winlinx@ya.ru>
2022-06-12 16:08:06 +0300

    Show download buttons for backup files without hover

--- src/routes/settings/backups/files/item.js
+++ src/routes/settings/backups/files/item.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import t from '~t'
 
-import { Item, ItemIcon, ItemTitle, ItemActions } from '~co/common/list'
+import { Item, ItemIcon, ItemTitle } from '~co/common/list'
 import Icon from '~co/common/icon'
 import { LongDateTime } from '~modules/format/date'
 import Button, { ButtonsGroup } from '~co/common/button'
@@ -12,19 +11,17 @@ export default function SettingsBackupsFilesItem({ created, files }) {
             <ItemIcon><Icon name='cache_ready' /></ItemIcon>
             <ItemTitle><LongDateTime date={created}/></ItemTitle>
 
-            <ItemActions>
-                <ButtonsGroup>
-                    <Button href={files.html} download>
-                        <Icon name='download' size='micro' />
-                        {t.s('download')} HTML
-                    </Button>
+            <ButtonsGroup>
+                <Button href={files.html} download>
+                    <Icon name='download' size='micro' />
+                    HTML
+                </Button>
 
-                    <Button href={files.csv} download>
-                        <Icon name='download' size='micro' />
-                        {t.s('download')} CSV
-                    </Button>
-                </ButtonsGroup>
-            </ItemActions>
+                <Button href={files.csv} download>
+                    <Icon name='download' size='micro' />
+                    CSV
+                </Button>
+            </ButtonsGroup>
         </Item>
     )
 }
