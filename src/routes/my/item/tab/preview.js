8244d63297bbd2d82172a78dcb9da31a7ff698ab
Rustem Mussabekov <winlinx@ya.ru>
2023-02-09 14:20:59 +0300

    New Preview endpoint

--- src/routes/my/item/tab/preview.js
+++ src/routes/my/item/tab/preview.js
@@ -1,7 +1,7 @@
 import s from './preview.module.styl'
 import React, { useState, useCallback } from 'react'
 import { useSelector } from 'react-redux'
-import { PREVIEW_URL } from '~data/constants/app'
+import { API_ENDPOINT_URL } from '~data/constants/app'
 
 import useWithWebView from '~co/highlights/useWithWebView'
 import Preloader from '~co/common/preloader'
@@ -18,16 +18,6 @@ export default function PageMyItemTabPreview({ item: { _id, type, link }, webVie
     const [loading, setLoading] = useState(true)
     const onLoad = useCallback(()=>{ setLoading(false) }, [setLoading])
 
-    //src
-    let src = ''
-    switch(type) {
-        case 'article':
-        case 'book':
-            src = PREVIEW_URL+'/article/'+btoa(link)+`#solid-bg=false&theme=${font_color||theme.app}&font-family=${font_family}&font-size=${font_size}`; break
-        default:
-            src = PREVIEW_URL+'/embed/'+btoa(link); break
-    }
-
     return (
         <>
             <iframe 
@@ -37,7 +27,7 @@ export default function PageMyItemTabPreview({ item: { _id, type, link }, webVie
                 allow='fullscreen; clipboard-write'
                 loading='eager'
                 referrerPolicy='no-referrer'
-                src={src}
+                src={`${API_ENDPOINT_URL}raindrop/preview/${_id}?type=${type}#solid-bg=false&theme=${font_color||theme.app}&font-family=${font_family}&font-size=${font_size}`}
                 onLoad={onLoad} />
 
             {loading && (
