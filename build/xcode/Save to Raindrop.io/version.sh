26ae528a2ddbd4400366776d1c97c60c6b9caa9d
Rustem Mussabekov <winlinx@ya.ru>
2023-02-20 12:06:23 +0300

    Run on ubuntu

--- build/xcode/Save to Raindrop.io/version.sh
+++ build/xcode/Save to Raindrop.io/version.sh
@@ -1,8 +1,12 @@
-PACKAGE_VERSION=$(cat ../../../package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-echo $PACKAGE_VERSION
-agvtool new-marketing-version $PACKAGE_VERSION
-agvtool next-version -all
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PACKAGE_VERSION=$(cat ../../../package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g')
+  echo $PACKAGE_VERSION
+  agvtool new-marketing-version $PACKAGE_VERSION
+  agvtool next-version -all
+else
+  echo "This script can only run on macOS!"
+fi
