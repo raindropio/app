cfe2f5cd688877b3647c608148b3447caff61a46
Rustem Mussabekov <winlinx@ya.ru>
2023-03-03 12:36:52 +0300

    Turn off extension welcome screen, on Safari appear always sometime

--- src/target/extension/background/runtime.js
+++ src/target/extension/background/runtime.js
@@ -1,12 +1,14 @@
 import browser from 'webextension-polyfill'
+import { environment } from '../environment'
 
 async function onInstalled({ reason }) {
     switch(reason) {
         case 'install':
-            await browser.tabs.create({
-                url: '/welcome/index.html',
-                active: true
-            })
+            if (!environment.includes('safari'))
+                await browser.tabs.create({
+                    url: '/welcome/index.html',
+                    active: true
+                })
             break
     }
 }
