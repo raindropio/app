2f6a111e329a71d287cc84c9d13b60673d961913
Rustem Mussabekov <winlinx@ya.ru>
2023-04-20 18:20:02 +0300

    Secure backup links

--- src/routes/settings/backups/files/item.js
+++ src/routes/settings/backups/files/item.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import { API_ENDPOINT_URL } from '~data/constants/app'
 
 import { Item, ItemIcon, ItemTitle } from '~co/common/list'
 import Icon from '~co/common/icon'
 import { LongDateTime } from '~modules/format/date'
 import Button, { ButtonsGroup } from '~co/common/button'
 
-export default function SettingsBackupsFilesItem({ created, files }) {
+export default function SettingsBackupsFilesItem({ _id, created }) {
     return (
         <Item>
             <ItemIcon><Icon name='cache_ready' /></ItemIcon>
             <ItemTitle><LongDateTime date={created}/></ItemTitle>
 
             <ButtonsGroup>
-                <Button href={files.html} download>
+                <Button href={`${API_ENDPOINT_URL}user/backup/${_id}/html`} download>
                     <Icon name='download' size='micro' />
                     HTML
                 </Button>
 
-                <Button href={files.csv} download>
+                <Button href={`${API_ENDPOINT_URL}user/backup/${_id}/csv`} download>
                     <Icon name='download' size='micro' />
                     CSV
                 </Button>
