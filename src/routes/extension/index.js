50080a45072d6f437e85e1f1659baa937a0a2cea
Rustem Mussabekov <winlinx@ya.ru>
2023-04-28 14:10:50 +0300

    Fix /extension web redirect

--- src/routes/extension/index.js
+++ src/routes/extension/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { target } from '~target'
-import { Navigate } from 'react-router-dom'
+import { Route, Navigate } from 'react-router-dom'
 
 let Component = target == 'extension' ? 
 	require('./index.extension').default :
-	function(){return <Navigate to='/' replace />}
+	function(){return <Route index element={<Navigate to='/' replace />} />}
 
 export default Component
