8e7707cded0e7f41fceb53f23257ebb9d6ce7700
Rustem Mussabekov <winlinx@ya.ru>
2023-04-28 12:56:28 +0300

    Redirect /extension route in web to root

--- src/routes/extension/index.js
+++ src/routes/extension/index.js
@@ -1,7 +1,9 @@
+import React from 'react'
 import { target } from '~target'
+import { Navigate } from 'react-router-dom'
 
 let Component = target == 'extension' ? 
 	require('./index.extension').default :
-	function(){return null}
+	function(){return <Navigate to='/' replace />}
 
 export default Component
