b5c5b85a679814856f0bbd542145a5151aa0e129
Rustem Mussabekov <winlinx@ya.ru>
2023-04-28 11:26:36 +0300

    Fix extension welcome screen

--- src/routes/extension/index.fallback.js
+++ src/routes/extension/index.fallback.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+
+import Protected from '../_protected'
 import Welcome from './welcome'
 
 export default ()=>{
     return (
-        <Route index element={<Welcome />} />
+        <Route element={<Protected redirect />}>
+            <Route index element={<Welcome />} />
+        </Route>
     )
 }
