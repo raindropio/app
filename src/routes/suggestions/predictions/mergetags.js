87edb4db467f0505ca513cec544c93d03c1490a0
Rustem Mussabekov <winlinx@ya.ru>
2024-05-14 13:53:30 +0300

    Support changing Merge tags suggestion

--- src/routes/suggestions/predictions/mergetags.js
+++ src/routes/suggestions/predictions/mergetags.js
@@ -25,15 +25,32 @@ function Destination({ _id, status, onApply }) {
     )
 }
 
-export default forwardRef(function MySuggestionsPredictionsMergetags({ prediction: { _id, tags }, status, onApply }, ref) {
+export default forwardRef(function MySuggestionsPredictionsMergetags({ prediction: { _id, tags }, status, onUpdate, onApply }, ref) {
+    //events
+    const onRemoveClick = useCallback(e=>{
+        e.preventDefault()
+        const index = parseInt(e.currentTarget.getAttribute('data-index'))
+        onUpdate({
+            _id,
+            tags: tags.filter((_,i)=>i != index)
+        })
+    }, [onUpdate, _id, tags])
+
+    if (!tags.length)
+        return null
+
     return (
         <div ref={ref} className={s.prediction} data-status={status}>
             <Destination _id={_id} status={status} onApply={onApply} />
 
             <ul>
-                {tags.map(([original, ...dups])=>(
+                {tags.map(([original, ...dups], index)=>(
                     <li key={original}>
-                        Replace {dups.map(tag=><React.Fragment key={tag}><s>#{tag}</s>, </React.Fragment>)} with <b>#{original}</b>
+                        <span>
+                            Replace {dups.map(tag=><React.Fragment key={tag}><s>#{tag}</s>, </React.Fragment>)} with <b>#{original}</b>
+                        </span>
+                        &nbsp;
+                        <a href='' data-index={index} onClick={onRemoveClick}><Icon name='close' size='micro' /></a>
                     </li>
                 ))}
             </ul>
