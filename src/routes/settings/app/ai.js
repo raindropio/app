1b55dc1195ea00955221b559605855357b58c70b
exentrich <winlinx@yandex.com>
2026-02-24 11:32:59 +0300

    Turn on/off for ai even for free

--- src/routes/settings/app/ai.js
+++ src/routes/settings/app/ai.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import t from '~t'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { set } from '~data/actions/config'
 import { isPro } from '~data/selectors/user'
@@ -13,21 +14,23 @@ function SettingsAppAi ({ ai_suggestions, ai_assistant, isPro, set }){
 			<Label>AI</Label>
 			<div>
 				<Checkbox 
-					disabled={!isPro}
-					checked={isPro && ai_assistant}
+					checked={ai_assistant}
 					onChange={()=>set('ai_assistant', !ai_assistant)}>
 					{t.s('ask')} AI
 					<a href={config.links.help.stella.index} target='_blank'>[?]</a>
 				</Checkbox>
 
 				<Checkbox 
-					disabled={!isPro}
-					checked={isPro && ai_suggestions}
+					checked={ai_suggestions}
 					onChange={()=>set('ai_suggestions', !ai_suggestions)}>
 					{t.s('suggested')} {t.s('collectionsCount').toLowerCase()} {t.s('und')} {t.s('tags').toLowerCase()}
 					<a href={config.links.help.suggestions.index} target='_blank'>[?]</a>
 				</Checkbox>
-				<SubLabel>You get your own private AI categorization model based on your data. Your data never used for training.</SubLabel>
+
+				<SubLabel>
+					{!isPro && <>Only available for <Link to='/settings/pro'>Pro</Link>. </>}
+					You get your own private AI categorization model based on your data. Your data is never used for training.
+				</SubLabel>
 			</div>
 		</>
 	)
